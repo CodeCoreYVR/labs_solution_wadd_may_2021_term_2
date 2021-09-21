72eed56977207329ff31a158ecd35fb370215f09
Ondřej Belza <belzaondra@gmail.com>
2021-09-21 14:48:56 -0700

    promises v1

---
+++ promises/math.js
@@ -0,0 +1,82 @@
+// Validating that all my inputs are numbers
+const validateInputs = (rejectFunction, ...inputs) => {
+  if (inputs.some((input) => isNaN(input))) {
+    rejectFunction(inputs.find((i) => isNaN(i)) + " is not a valid number'");
+  }
+};
+
+const add = (...numbers) =>
+  new Promise((resolve, reject) => {
+    validateInputs(reject, ...numbers);
+    setTimeout(() => {
+      resolve(numbers.reduce((prev, cur) => prev + cur, 0));
+    }, 10);
+  });
+
+const mult = (...nums) =>
+  new Promise((resolve, reject) => {
+    validateInputs(reject, ...nums);
+    setTimeout(() => {
+      resolve(
+        nums.reduce((prev, cur, index) => {
+          if (index == 0) return prev;
+          else return prev * cur;
+        }, nums[0])
+      );
+    }, 10);
+  });
+
+const div = (...nums) =>
+  new Promise((resolve, reject) => {
+    validateInputs(reject, ...nums);
+    setTimeout(() => {
+      resolve(
+        nums.reduce((prev, cur, index) => {
+          if (index == 0) return prev;
+          else return prev / cur;
+        }, nums[0])
+      );
+    }, 10);
+  });
+
+const sub = (...nums) =>
+  new Promise((resolve, reject) => {
+    validateInputs(reject, ...nums);
+    setTimeout(() => {
+      resolve(
+        nums.reduce((prev, cur, index) => {
+          if (index == 0) return prev;
+          else return prev - cur;
+        }, nums[0])
+      );
+    }, 10);
+  });
+
+const pow = (...nums) =>
+  new Promise((resolve, reject) => {
+    validateInputs(reject, ...nums);
+    setTimeout(() => {
+      resolve(
+        nums.reduce((prev, cur, index) => {
+          if (index == 0) return prev;
+          else return Math.pow(prev, cur);
+        }, nums[0])
+      );
+    }, 10);
+  });
+
+add(9)
+  .then((r) => add(9, r))
+  .then((r) => add(r, 2))
+  .then((r) => mult(5, r))
+  .then((r) => div(r, 10))
+  .then(console.log)
+  .catch(console.error);
+
+pow(2)
+  .then((r) => pow(2, r)) // r is 2
+  .then((r) => pow(2, r)) // r is 4
+  .then((r) => mult("fitty-two", r)) // r is 16, but the mult('fitty-two', ...)
+  // should reject
+  .then(console.log) // never makes it here
+  .catch(console.error); // error logs 'fitty-two is not a valid number'
---
+++ promises/test.js
@@ -0,0 +1,36 @@
+// const add = (num1, num2 = 0) => {
+//   console.log(num1, num2);
+// };
+
+// add(9, 5);
+
+//Example of reduce function
+const numbers = [1, 2, 3, 4];
+console.log(
+  numbers.reduce((prevValue, currentValue) => {
+    console.log(prevValue, currentValue);
+
+    return prevValue + currentValue;
+  }, 0)
+);
+
+// Same as .join("")
+const joinWords = (wordsArray) => {
+  return wordsArray.reduce((prev, cur) => {
+    return prev + cur;
+  }, "");
+};
+
+// add function with using rest operator
+const add = (...rest) => {
+  return rest.reduce((a, b) => a + b, 0);
+};
+
+// Example
+const add = (num1, num2, num3) => {
+  return num1 + num2 + num3;
+};
+// console.log(add(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+//Test of join words array function
+console.log(joinWords(["test", "test2", " another string"]));
