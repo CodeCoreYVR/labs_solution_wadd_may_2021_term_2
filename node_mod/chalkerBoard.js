85adbf162db3f0c77db590a6e5b577b2381572f0
Ondrej Belza <belzaondra@gmail.com>
2021-09-26 11:38:52 -0700

    chalk

---
+++ node_mod/chalkerBoard.js
@@ -0,0 +1,28 @@
+// Importing chalk module
+const chalk = require("chalk");
+
+// getting color from argv
+let color = process.argv[2];
+
+// formatting to correct function name
+const bgColor = "bg" + color[0].toUpperCase() + color.slice(1);
+
+// Getting width from argv
+const width = process.argv[3];
+
+// Getting height from argv
+const height = process.argv[4];
+
+// Creating loop for specific amount of rows
+for (let row = 1; row <= height; row++) {
+  // Creating empty string for current row
+  let rowText = "";
+  // Creating loop for current rows columns
+  for (let column = 1; column <= width; column++) {
+    // Checking if column + row is even and based on that adding coloured background or white
+    if ((column + row) % 2) rowText += chalk[bgColor](" ");
+    else rowText += " ";
+  }
+  // Console logging current rows text
+  console.log(rowText);
+}
