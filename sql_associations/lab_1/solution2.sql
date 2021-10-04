2a4835c459a4bbe40ff27ffd85789c2bf162fd59
Ondřej Belza <belzaondra@gmail.com>
2021-10-04 15:03:47 -0700

    lab 2 - part 1

--- sql_associations/lab_1/solution2.sql
+++ sql_associations/lab_1/solution2.sql
@@ -1,6 +1,9 @@
 -- Select product names, product prices and the lowest price they were sold at during the last 20 months.
 SELECT products.id, products.name, products.price, ROUND(CAST(products.sale_price as numeric), 2),
 (
-    SELECT ROUND(CAST(MIN(line_items.price) as numeric), 2) as min_price FROM line_items WHERE product_id = products.id
+    SELECT ROUND(CAST(MIN(line_items.price) as numeric), 2) as min_price
+    FROM line_items
+    WHERE product_id = products.id AND
+    created_at > '2018-01-04'
 )
 FROM products;
