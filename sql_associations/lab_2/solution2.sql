fafd72b1aeefee45ed05c3278444a38f4d16096a
Ondřej Belza <belzaondra@gmail.com>
2021-10-04 16:01:54 -0700

    fix bug

--- sql_associations/lab_2/solution2.sql
+++ sql_associations/lab_2/solution2.sql
@@ -1,5 +1,9 @@
 -- Find the average order total price for all the orders in the system
-SELECT order_id, SUM(price) as total_price
-FROM line_items 
-GROUP BY order_id
-ORDER BY order_id;
+SELECT AVG(order_price) 
+FROM (
+    SELECT SUM(price) as order_price
+        FROM line_items 
+        GROUP BY order_id
+        ORDER BY order_id
+) as res
+        
