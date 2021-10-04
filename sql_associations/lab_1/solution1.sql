c60d91cf5ad39bfd04ac2459ac4d5c476ef87915
Ondřej Belza <belzaondra@gmail.com>
2021-10-04 14:35:49 -0700

    solution2

--- sql_associations/lab_1/solution1.sql
+++ sql_associations/lab_1/solution1.sql
@@ -1,6 +1,7 @@
 -- Find the average line_item total price (meaning quantity * price) for each order that were completed in January 2016 month.
 
-SELECT product_id, AVG(quantity * price) AS average_price FROM orders 
+SELECT product_id, AVG(quantity * price) AS average_price
+FROM orders 
 INNER JOIN line_items 
 ON orders.id = line_items.order_id
 WHERE orders.completed_on > '2016-01-01'
